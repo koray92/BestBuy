09af9b6d6c7954a6d0b993e615d851e782f7a983
koray92 <112970299+koray92@users.noreply.github.com>
2025-02-07 13:37:38 +0100

    create product class

---
+++ products.py
@@ -0,0 +1,54 @@
+class Product:
+
+    def __init__(self, name: str, price: float, quantity: int):
+        try:
+            if name == "" or price <= 0 or quantity <= 0:
+                raise Exception
+            self.name = name
+            self.price = price
+            self.quantity = quantity
+            self.active = True
+
+        except Exception as e:
+            print(e)
+
+
+    def get_quantity(self):
+        return self.quantity
+
+
+    def set_quantity(self, quantity):
+        self.quantity = quantity
+        if self.quantity <= 0:
+            self.deactivate()
+
+
+
+    def is_active(self):
+        if self.active:
+            return True
+        else:
+            return False
+
+
+    def activate(self):
+        self.active = True
+
+
+    def deactivate(self):
+        self.active = False
+
+
+    def show(self):
+        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
+
+
+    def buy(self, quantity):
+        try:
+            if self.quantity < quantity:
+                raise ValueError
+            total_price = self.price * self.quantity
+            self.quantity -= quantity
+            return f"Total price of purchase: {total_price}"
+        except ValueError as v:
+            print(v)
